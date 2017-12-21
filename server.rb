84cc175ca372d024da6e2c83b3a3e8b720265ced
Dominick Del Bosque <dominick@theosfp.com>
2017-12-20 16:43:54 -0800

    Configured namespacing with Sinatra

--- server.rb
+++ server.rb
@@ -1,15 +1,21 @@
 require 'sinatra'
+require 'sinatra/namespace'
 require 'mongoid'
 
+# DB Setup
 Mongoid.load! 'mongoid.config'
 
+# Models
 class Restaurant
 	include Mongoid::Document
 
 	field :name, type: String
-
 end
 
+# Endpoints
 get '/' do
 	'Welcome to On My Wait List'
+end
+
+namespace '/api/v1' do
 end
